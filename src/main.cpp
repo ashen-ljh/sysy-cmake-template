dbb418c2b826f2d22eb119378c1317fe13491254
ashen-ljh <114344872+ashen-ljh@users.noreply.github.com>
2024-01-12 03:29:12 +0800

    test

--- src/main.cpp
+++ src/main.cpp
@@ -1,64 +1,59 @@
 #include <cassert>
 #include <cstdio>
 #include <iostream>
+#include <sstream>
 #include <memory>
 #include <string>
-#include "AST.hpp"
-#include "riscv.hpp"
+#include "AST.h"
+#include "RISCV.h"
 #include "koopa.h"
+#define _SUB_MODE
 using namespace std;
 
-// 声明 lexer 的输入, 以及 parser 函数
-// 为什么不引用 sysy.tab.hpp 呢? 因为首先里面没有 yyin 的定义
-// 其次, 因为这个文件不是我们自己写的, 而是被 Bison 生成出来的
-// 你的代码编辑器/IDE 很可能找不到这个文件, 然后会给你报错 (虽然编译不会出错)
-// 看起来会很烦人, 于是干脆采用这种看起来 dirty 但实际很有效的手段
+
 extern FILE *yyin;
 extern int yyparse(unique_ptr<BaseAST> &ast);
 
-int main(int argc, const char *argv[]) {
-  // 解析命令行参数. 测试脚本/评测平台要求你的编译器能接收如下参数:
-  // compiler 模式 输入文件 -o 输出文件
-  assert(argc == 5);
-  auto mode = argv[1];
-  auto input = argv[2];
-  auto output = argv[4];
 
-  // 打开输入文件, 并且指定 lexer 在解析的时候读取这个文件
-  yyin = fopen(input, "r");
-  assert(yyin);
+int main(int argc, const char *argv[])
+{
+    assert(argc == 5);
+    auto mode = argv[1];
+    auto input = argv[2];
+    auto output = argv[4];
 
-  // 调用 parser 函数, parser 函数会进一步调用 lexer 解析输入文件的
-  unique_ptr<BaseAST> ast;
-  auto ret = yyparse(ast);
-  assert(!ret);
-  if(mode[1]=='k')
-  {
-    freopen(output,"w",stdout);
+    yyin = fopen(input, "r");
+    #ifdef  _SUB_MODE
+    freopen(output, "w", stdout); 
+    #endif
+    assert(yyin);
 
-    ast->Dump();
+    unique_ptr<BaseAST> ast;
+    auto ret = yyparse(ast);
+    assert(!ret);
+
+    if (string(mode) == "-koopa")ast->dumpIR();
+    else if (string(mode) == "-riscv" || string(mode) == "-perf")
+    {
+        stringstream ss;
+        streambuf* cout_buf = cout.rdbuf();
+        cout.rdbuf(ss.rdbuf());
+        ast->dumpIR();
+        string ir_str = ss.str();
+        const char *ir = ir_str.data();
+        cout.rdbuf(cout_buf);
+        koopa_program_t program;
+        koopa_error_code_t ret = koopa_parse_from_string(ir, &program);
+        assert(ret == KOOPA_EC_SUCCESS);
+        koopa_raw_program_builder_t builder = koopa_new_raw_program_builder();
+        koopa_raw_program_t raw = koopa_build_raw_program(builder, program);
+        koopa_delete_program(program);
+        freopen(output, "w", stdout);
+        Visit(raw);
+        koopa_delete_raw_program_builder(builder);
+    }
+    else if (string(mode) == "-test")ast->dump();
+    else cout << "NotImplementedError" << endl;
+    cout << endl;
     return 0;
-  }
-  freopen("whatever.txt","w",stdout);
-
-  ast->Dump();
-  cout<<endl;
-  FILE* ff=fopen("whatever.txt","r");
-  char *buf=(char *)malloc(10000000);
-  fread(buf, 1,10000000, ff);
-  //cout<<buf;
-  /*freopen("temps.txt","w",stdout);
-  parse_string(buf,0);
-  M.clear();
-  array_size.clear();
-  deep=0;
-  now_array=0;*/
-  freopen(output,"w",stdout);
-  parse_string(buf);
-    
-    return 0;
-
-  
-
-  
 }
